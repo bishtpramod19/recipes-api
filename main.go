4bdf654e50a6cbe2a899117f3f9b8406b20ca9f4
bishtpramod19 <bisht.pramod19@gmail.com>
2023-04-12 13:30:13 +0530

    added recipe data model

--- main.go
+++ main.go
@@ -3,6 +3,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Recipe struct {
+
+	Name string `json:"name"`
+	Tags []string `json:"tags"`
+	Ingredients []string `json:"ingredients"`
+	Instructions []string `json:"instructions"`
+	PublishedAt time.time `json:"publishedAt"`
+
+}
+
 func main(){
 	router := gin.Default()
 	router.Run()
