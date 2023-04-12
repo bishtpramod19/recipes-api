ceef1b018c739fd8414ceefe389cb1d2da93560d
Pramod Singh Bisht -X (prambish - TATA CONSULTANCY SERVICES LIMITED at Cisco) <prambish@cisco.com>
2023-04-12 16:52:34 +0530

    added new recipe endpoint

--- main.go
+++ main.go
@@ -1,19 +1,46 @@
 package main
+
 import (
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
 
-type Recipe struct {
+var recipes []Recipe
 
-	Name string `json:"name"`
-	Tags []string `json:"tags"`
-	Ingredients []string `json:"ingredients"`
-	Instructions []string `json:"instructions"`
-	PublishedAt time.time `json:"publishedAt"`
+func init() {
+	recipes = make([]Recipe, 0)
+}
+
+type Recipe struct {
+	Id           string    `json:"id"`
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
+}
+
+func NewRecipeHandler(c *gin.Context) {
+
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+
+	}
+
+	recipe.PublishedAt = time.Now()
+	recipe.Id = xid.New().String()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipe)
 
 }
 
-func main(){
+func main() {
 	router := gin.Default()
+	router.POST("/recipes", NewRecipeHandler)
 	router.Run()
 }
